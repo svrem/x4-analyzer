f6620c2c5a17fddf6770464ff4af787f1b04f452
svrem <svrem@icloud.com>
2024-12-27 21:14:11 +0100

    feat: init

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Station struct {
+	ID    string
+	Code  string
+	Owner string
+	Name  string
+}
+
+type StationPageData struct {
+	Stations []Station
+}
+
+type TradeOffer struct {
+	Offertype string
+	Ware      string
+	Price     float32
+	Amount    float32
+}
+
+type TradeOfferPageData struct {
+	Station     Station
+	TradeOffers []TradeOffer
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/stations", func(w http.ResponseWriter, r *http.Request) {
+		stationsTemplate := template.Must(template.ParseFiles("views/stations.html"))
+
+		stations, err := db.Query("SELECT * FROM stations")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stations.Close()
+
+		var stationList []Station
+		for stations.Next() {
+			var id string
+			var code string
+			var owner string
+			var name string
+			stations.Scan(&id, &code, &owner, &name)
+
+			station := Station{
+				ID:    id,
+				Code:  code,
+				Owner: owner,
+				Name:  name,
+			}
+			stationList = append(stationList, station)
+		}
+
+		data := StationPageData{
+			Stations: stationList,
+		}
+
+		stationsTemplate.Execute(w, data)
+	})
+
+	http.Handle("/stations/{id}", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		stationTemplate := template.Must(template.ParseFiles("views/station.html"))
+
+		id := r.PathValue("id")
+
+		stations, err := db.Query("SELECT code, owner FROM stations WHERE id = ?", id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stations.Close()
+
+		if !stations.Next() {
+			http.NotFound(w, r)
+			return
+		}
+
+		var code string
+		var owner string
+		stations.Scan(&code, &owner)
+
+		station := Station{
+			ID:    id,
+			Code:  code,
+			Owner: owner}
+
+		fmt.Println("Station: ", station)
+
+		tradeOffers, err := db.Query("SELECT * FROM tradeoffers WHERE station_id = ?", id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer tradeOffers.Close()
+
+		var tradeOfferList []TradeOffer
+		for tradeOffers.Next() {
+			var offertype string
+			var ware string
+			var price float32
+			var amount float32
+			var station_id string
+
+			tradeOffers.Scan(&offertype, &ware, &price, &amount, &station_id)
+
+			tradeOffer := TradeOffer{
+				Offertype: offertype,
+				Ware:      ware,
+				Price:     price,
+				Amount:    amount}
+			tradeOfferList = append(tradeOfferList, tradeOffer)
+
+		}
+
+		data := TradeOfferPageData{
+			Station:     station,
+			TradeOffers: tradeOfferList,
+		}
+
+		fmt.Println("TradeOffers: ", tradeOfferList)
+
+		stationTemplate.Execute(w, data)
+	}))
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ parse.py
@@ -0,0 +1,71 @@
+import sqlite3
+import xml.etree.ElementTree as ET
+
+import os
+if os.path.exists('data.db'):
+    os.remove('data.db')
+
+
+conn = sqlite3.connect('data.db')
+cursor = conn.cursor()
+
+tree = ET.parse('save.xml')
+root = tree.getroot()
+
+def insert_stations():
+    cursor.execute("CREATE TABLE IF NOT EXISTS stations (id TEXT PRIMARY KEY, code TEXT, owner TEXT, name TEXT)")
+    cursor.execute("CREATE TABLE tradeoffers (type TEXT, ware TEXT, price INTEGER, amount INTEGER, station_id TEXT, FOREIGN KEY(station_id) REFERENCES stations(id))")
+
+    stations = root.findall(".//component[@class='station']")
+    station_data = []
+    trade_offer_data = []
+
+    for station in stations:
+        trade_offers = station.findall(".//offers//trade")
+        
+        for trade_offer in trade_offers:
+            is_buyer = trade_offer.get('buyer') is not None
+            ware = trade_offer.get('ware')
+            price = trade_offer.get('price')
+            amount = trade_offer.get('amount')
+
+            if ware is None:
+                continue
+
+            trade_offer_data.append(("buyoffer" if is_buyer else "selloffer", ware, price, amount, station.get('id')))
+
+
+        id = station.get('id')
+        code = station.get('code')
+        owner = station.get('owner')
+        name = station.get('name')
+        station_data.append((id, code, owner, name))
+
+    cursor.executemany("INSERT INTO stations (id, code, owner, name) VALUES (?, ?, ?, ?)", station_data)
+    cursor.executemany("INSERT INTO tradeoffers (type, ware, price, amount, station_id) VALUES (?, ?, ?, ?, ?)", trade_offer_data)
+
+# def insert_tradeoffers():
+#     cursor.execute("CREATE TABLE IF NOT EXISTS tradeoffers (type TEXT, time INTEGER, owner TEXT, ware TEXT, price INTEGER, v INTEGER, t2 INTEGER, price2 INTEGER, v2 INTEGER)")
+
+#     tradeoffer_entries = root.findall(".//log[@type='buyoffer']") + root.findall(".//log[@type='selloffer']")
+#     tradeoffer_data = []
+
+#     for entry in tradeoffer_entries:
+#         offer_type = entry.get('type')
+#         time = entry.get('time')
+#         owner = entry.get('owner')
+#         ware = entry.get('ware')
+#         price = entry.get('price')
+#         v = entry.get('v')
+#         t2 = entry.get('t2')
+#         price2 = entry.get('price2')
+#         v2 = entry.get('v2')
+#         tradeoffer_data.append((offer_type, time, owner, ware, price, v, t2, price2, v2))
+
+#     cursor.executemany("INSERT INTO tradeoffers (type, time, owner, ware, price, v, t2, price2, v2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", tradeoffer_data)
+
+insert_stations()
+
+conn.commit()
+conn.close()
+
